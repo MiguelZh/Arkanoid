a89fe9362403ef7d5128f5d0935e3c68cc4d35d5
MiguelZh <37276857+MiguelZh@users.noreply.github.com>
2018-12-04 16:59:10 +0100

    algo del menu

---
+++ HolaSDL/Menu.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "SDL_image.h"
+#include "Texture.h"
+#include "checkML.h"
+
+const int MENU_WIDTH = 800;
+const int MENU_HEIGHT = 600;
+
+class Menu {
+private:
+	const enum textureName { titleText, playButtonText,quitText,loadButtonText};
+
+	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+	SDL_Rect titleRect;
+	Texture * titleTexture = nullptr;
+	SDL_Rect playButtonRect;
+	Texture * playTexture = nullptr;
+	SDL_Rect QuitRect;
+	Texture * quitTexture = nullptr;
+	SDL_Rect loadButtonRect;
+	Texture * loadTexture = nullptr;
+	bool exit = false;
+public:
+	~Menu();
+	Menu();
+	void render();
+	void update();
+	void handleEvents();
+};
