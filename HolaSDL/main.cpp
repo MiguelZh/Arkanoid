4adae41cf84acd57f02176933612405892fdc412
MiguelZh <37276857+MiguelZh@users.noreply.github.com>
2018-12-04 20:19:11 +0100

    menu 1.0

--- HolaSDL/main.cpp
+++ HolaSDL/main.cpp
@@ -1,46 +1,24 @@
 
 
+
+
 #include <iostream>
 #include "Game.h"
-
+#include "Menu.h"
 using namespace std;
 
 using uint = unsigned int;
-void Menu() {
-	//_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // Check Memory Leaks
-	SDL_Window* window = nullptr;
-	SDL_Renderer* renderer = nullptr;
-	SDL_Surface *windowSurface = nullptr;
 
-	const uint winWidth = 800;
-	const uint winHeight = 600;
-	SDL_Init(SDL_INIT_EVERYTHING);
-	window = SDL_CreateWindow("Menu", SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED, winWidth, winHeight, SDL_WINDOW_SHOWN);
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-	if (window == nullptr || renderer == nullptr)
-		cout << "Error cargando SDL" << endl;
-
-	else {
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-		SDL_RenderClear(renderer);
-		SDL_RenderPresent(renderer);
-		SDL_Delay(5000);
-	}
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-}
-
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[]) {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); // Check Memory Leaks
-	//Menu();
+	Menu menu;
+	menu.run();
 	try {
-		Game game;
-		game.run();		
+		Game  game;
+		game.run();
 	}
-	catch(exception e){
+	catch (exception e) {
 		cout << e.what();
 	}
 	return 0;
